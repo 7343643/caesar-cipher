42fcf3db72563ab579a9203093cfec5e40c3c346
7343643 <133917446+7343643@users.noreply.github.com>
2023-08-15 11:55:32 +1200

    Add files via upload

--- caesar cipher/cipherv2.py
+++ caesar cipher/cipherv2.py
@@ -6,15 +6,20 @@ def cipher(shift, message):
     result = ""
     #makes a variable called alphabet which contains the alphabet
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    
+    #repeats the code contain for each letter inside the variable message
     for letter in message:
+        #only runs the code contained if the letter is in the variable alphabet
         if letter in alphabet:
+            #finds the position of the letter in the variable alphabet and assigns it to a variable called index
             index = alphabet.find(letter)
             index = (index + shift)
             if index > 25:
-                index = index - 26
-            
-            
+                while index > 25:
+                    index = index - 26
+            if index < -25:
+                while index < -25:
+                    index = index + 26
             result = result + alphabet[index]
+        
         else:
             result = result + letter
